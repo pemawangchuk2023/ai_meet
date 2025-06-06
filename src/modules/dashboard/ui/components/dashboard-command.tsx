738142b0d4165d3eebac085c5ff804f1e24ee1c0
Pema Wangchuk <pemawangchug1986@gmail.com>
2025-06-06 23:38:45 +0600

    added navbar

---
+++ src/modules/dashboard/ui/components/dashboard-command.tsx
@@ -0,0 +1,29 @@
+import React, { Dispatch, SetStateAction } from "react";
+import {
+	Command,
+	CommandDialog,
+	CommandEmpty,
+	CommandGroup,
+	CommandInput,
+	CommandItem,
+	CommandList,
+	CommandSeparator,
+	CommandShortcut,
+} from "@/components/ui/command";
+
+interface Props {
+	open: boolean;
+	setOpen: Dispatch<SetStateAction<boolean>>;
+}
+const DashboardCommand = ({ open, setOpen }: Props) => {
+	return (
+		<CommandDialog open={open} onOpenChange={setOpen}>
+			<CommandInput placeholder='Find a meeting or agent' />
+			<CommandList>
+				<CommandItem>Test</CommandItem>
+			</CommandList>
+		</CommandDialog>
+	);
+};
+
+export default DashboardCommand;
--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -8,14 +8,26 @@ import {
 	DropdownMenuSeparator,
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import {
+	Drawer,
+	DrawerContent,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "@/components/ui/drawer";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { authClient } from "@/lib/auth-client";
 import GeneratedAvatar from "@/components/generated-avatar";
 import { ChevronDown, CreditCardIcon, LogOutIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useIsMobile } from "@/hooks/use-mobile";
+import { Button } from "@/components/ui/button";
 
 const DashboardUserButton = () => {
 	const router = useRouter();
+	const isMobile = useIsMobile();
 	const { data, isPending } = authClient.useSession();
 
 	if (isPending || !data?.user) {
@@ -30,6 +42,49 @@ const DashboardUserButton = () => {
 			},
 		});
 	};
+	if (isMobile) {
+		return (
+			<Drawer>
+				<DrawerTrigger asChild>
+					<div className='rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden'>
+						{data.user.image ? (
+							<Avatar>
+								<AvatarImage src={data.user.image} alt='User avatar' />
+							</Avatar>
+						) : (
+							<GeneratedAvatar
+								seed={data.user.name}
+								variant='initials'
+								className='size-9 mr-3'
+							/>
+						)}
+						<div className='flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0'>
+							<p className='text-sm truncate w-full cursor-pointer'>
+								{data.user.name}
+							</p>
+							<p className='text-xs truncate w-full'>{data.user.email}</p>
+						</div>
+						<ChevronDown className='size-4 shrink-0' />
+					</div>
+				</DrawerTrigger>
+				<DrawerContent>
+					<DrawerHeader>
+						<DrawerTitle>{data.user.name}</DrawerTitle>
+						<DrawerDescription>{data.user.email}</DrawerDescription>
+					</DrawerHeader>
+					<DrawerFooter>
+						<Button variant='outline' onClick={() => {}}>
+							<CreditCardIcon className='size-4 text-black' />
+						</Button>
+						<Button variant='outline' onClick={onLogout}>
+							<LogOutIcon className='size-4 text-black' />
+						</Button>
+					</DrawerFooter>
+				</DrawerContent>
+			</Drawer>
+		);
+	}
+
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger className='rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden'>
@@ -63,6 +118,7 @@ const DashboardUserButton = () => {
 				</DropdownMenuLabel>
 				<DropdownMenuSeparator />
 				<DropdownMenuItem className='cursor-pointer flex items-center justify-between'>
+					{/* Todo */}
 					Billing
 					<CreditCardIcon />
 				</DropdownMenuItem>
