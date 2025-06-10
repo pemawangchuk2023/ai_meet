317166c0f32382058cc142bccb54b99b4ea289bc
Pema Wangchuk <pemawangchug1986@gmail.com>
2025-06-10 14:52:39 +0600

    implement agent setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Fira_Code } from "next/font/google";
 import "./globals.css";
+import { TRPCReactProvider } from "@/trpc/client";
 
 const inter = Inter({
 	variable: "--font-inter",
@@ -25,10 +26,12 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
-			<body className={`${inter.variable} ${firaCode.variable} antialiased`}>
-				{children}
-			</body>
-		</html>
+		<TRPCReactProvider>
+			<html lang='en'>
+				<body className={`${inter.variable} ${firaCode.variable} antialiased`}>
+					{children}
+				</body>
+			</html>
+		</TRPCReactProvider>
 	);
 }
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,34 +1,9 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
 import React from "react";
 
 const HomeView = () => {
-	const { data: session } = authClient.useSession();
-	const router = useRouter();
-
-	if (!session) {
-		return <p>Loading...</p>;
-	}
-	return (
-		<div className='flex flex-col p-4 gap-y-4'>
-			<p>Logged In as {session?.user.name}</p>
-			<Button
-				onClick={() =>
-					authClient.signOut({
-						fetchOptions: {
-							onSuccess: () => router.push("/sign-in"),
-						},
-					})
-				}
-				className='cursor-pointer'
-			>
-				Sign Out
-			</Button>
-		</div>
-	);
+	return <div>Home View</div>;
 };
 
 export default HomeView;
